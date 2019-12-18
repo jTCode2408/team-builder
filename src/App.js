739f1408c421bc2f15e94f2611287601b8938dd4
Jasmine Terry <jasmine.terry91@gmail.com>
2019-12-18 16:37:44 -0500

    TeamMember component set up. Props showing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Form from './Form';
 import './App.css';
+import TeamMember from './TeamMember';
 
 //Import the useState hook and set up a state property for your team members list
 //Give the state variable you just declared a default value. You will need to keep track of a list of team members and each team member will have several key/value pairs associated with them
@@ -10,32 +11,31 @@ function App() {
 
 const [teamMembers, setTeamMembers] = useState([
   {
-      name: 'Jasmine',
-      email: 'jasmine.tery91@gmail.com',
-      role: 'Full stack web dev'
+      id:1,
+      name: "Jasmine",
+      email: "jasmine.tery91@gmail.com",
+      role: "Full stack web dev"
 }
 ]); //base TM info
 
 //function for new TM's
-const addNewTM = teamMember =>{
-  const newTeamMember ={
-    name: teamMember.name,
-    email:teamMember.email,
-    role: teamMember.role
+const addNewTM = tm => {
+  const newTeamMember = {
+    id: Date.now(),
+    name: tm.name,
+    email:tm.email,
+    role: tm.role
   };
   setTeamMembers([...teamMembers, newTeamMember]);
 };
-
+ //set TM , spread team memberrs & add new team member to it
 
   return (
     <div className="App">
-      <header className="App-header">
         <h1> Team Members! </h1>
-      </header>
 {/* 
-<Form addNewTM ={addNewTM} /> */}
-{/* <teamMembers /> */}
-
+<Form addNewTM = {addNewTM} /> */}
+<TeamMember teamMembers ={teamMembers} />
 
     </div>
   );
--- src/TeamMember.js
+++ src/TeamMember.js
@@ -4,9 +4,23 @@ import React from 'react';
 //return name, email, role for every TM
 
 const TeamMember = props =>{
+    console.log(props);
 return (
-<div></div>
-)
+<div className = "teamList">
 
-}
+{/* {props.teamMember.map( teamMember => (
+    <div className ="member"
+    key= {teamMember.id}>
 
+   <h2>{teamMember.name}</h2>
+    <h3>{teamMember.email}</h3>
+    <h3>  {teamMember.role}</h3>
+</div>
+))} */}
+
+</div>
+);
+
+};
+
+export default TeamMember;
